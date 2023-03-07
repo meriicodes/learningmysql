58cd208032ee95879c90018674e202e48128d5b6
Maria Clara <117320230+meriicodes@users.noreply.github.com>
2023-03-07 17:40:50 -0300

    Update main.sql

--- main.sql
+++ main.sql
@@ -9,3 +9,27 @@ Peso FLOAT NULL,
 Nacionalidade VARCHAR(30) default 'teste');
 
 drop table alunos;
+database ESTUDO DIRIGIDO;
+/*  Tive que criar um novo esquema e tabela, já que havia um database com mesmo nome no computador*/ 
+
+CREATE SCHEMA IF NOT EXISTS ESTUDO;
+CREATE TABLE IF NOT EXISTS ESTUDO. estudante (/* criando a tabela alunos */
+idestudante int not null auto_increment, /*definindo o campo chave primaria */
+Nome VARCHAR(40) NOT NULL,
+Nascimento date,
+Sexo enum ('m', 'f'),
+Altura decimal (5,2),
+Peso decimal(5,2),
+Nacionalidade VARCHAR(30),
+primary key (idestudante)
+) 
+
+CREATE TABLE IF NOT EXISTS ESTUDO.prof (/* criando a tabela alunos */
+idestudante int not null auto_increment, /*definindo o campo chave primaria */ 
+
+Nome VARCHAR(40) NOT NULL, /* nome do professor*/
+Nascimento date, /* data de nascimento professor*/
+Sexo enum ('m', 'f'), /* gênero do professor*/
+Nacionalidade VARCHAR(30), /*nacionalidade do professor */
+primary key (idprof) /* chave primária serve como único identificador da tabela, são representadas por aqueles campos que não recebem valores repetidos*/
+)
